6fb5439426b67cc196d66b0fc9326d0eff1b136f
Justslugard <bot3445x@gmail.com>
2025-10-24 18:40:19 +0700

    I FORGOT TO COMMIT

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 let y = 200;
 let x = 200;
 let vy = 0;
-const pvx = 2;
+let pvx = 2;
 let isPlaying = false;
+let THEscore = 0;
 
-pipes = [
+let pipes = [
    [400, Math.floor(Math.random() * 150)],
    [500, Math.floor(Math.random() * 150)],
    [600, Math.floor(Math.random() * 150)],
@@ -12,23 +13,28 @@ pipes = [
    [800, Math.floor(Math.random() * 150)]
 ]
 
+let pipesChace = [];
+
 const canv = document.getElementById("canv");
 const ctx = canv.getContext("2d");
 
 const score = document.getElementById("score");
+const narrate = document.getElementById("narrate");
 
 const jumpSfx = new Audio();
 jumpSfx.src = "audio/jump.mp3";
 
 const image = new Image();
-image.src="img/birdie2.png";
+image.src = "img/birdie2.png";
 
 const overSfx = new Audio();
 overSfx.src = "audio/over.mp3"
 
 // setInterval(game, 1000/30);
 
+let trueScore = 0;
 function game() {
+  let scoreNums = Number(score.textContent.split(" ")[1]);
   ctx.clearRect(0,0,canv.width,canv.height);
   vy += 0.3;
   y += vy;
@@ -36,7 +42,6 @@ function game() {
   ctx.drawImage(image, x, y, 40, 40);
 
   for (i = 0; i < pipes.length; i++) {
-    ctx.strokeStyle = "black";
     ctx.fillStyle = "lightgreen";
     ctx.fillRect(pipes[i][0], 0, 50, pipes[i][1]);
     ctx.strokeRect(pipes[i][0], 0, 50, pipes[i][1]);
@@ -49,27 +54,41 @@ function game() {
     ctx.fillRect(pipes[i][0] - 10, pipes[i][1] + 150, 70, 20);
     ctx.strokeRect(pipes[i][0] - 10, pipes[i][1] + 150, 70, 20);
 
-    if (pipes[i][0] < 240 && pipes[i][0] > 160 && (pipes[i][1] > y || pipes[i][1] + 150 <= y + 30)) {
-      overSfx.play();
-      alert("Birdie ded, Skill issue");
-      window.location.reload();
-      return;
-    }
+    // if (pipes[i][0] < 240 && pipes[i][0] > 160 && (pipes[i][1] > y || pipes[i][1] + 150 <= y + 30)) {
+    //   overSfx.play();
+    //   alert("Birdie ded, Skill issue");
+    //   window.location.reload();
+    //   return;
+    // }
 
-    if (pipes[i][0] == x) {
-      score.textContent = `SCORE: ${Number(score.textContent.split(" ")[1]) + 1}`;
+    if (pipes[i][0] > (x - 17) && pipes[i][0] < x && !pipesChace.includes(pipes[i][1])) {
+      pipesChace.pop();
+      score.textContent = `SCORE: ${scoreNums + 1}`;
+      pipesChace.push(pipes[i][1]);
     }
 
     if (pipes[i][0] > -50) {
       pipes[i][0] -= pvx;
     } else {
+      trueScore += 1;
+      console.log(trueScore, scoreNums, `OFFSET: ${scoreNums - trueScore}`, `SPEED: ${pvx}`);
       pipes[i][0] = 450;
       pipes[i][1] = Math.floor(Math.random() * 150);
     }
   }
 
   if(y > 400)
-    vy = -10;
+    vy =-10;
+
+  if (scoreNums % 100 === 0 && scoreNums !== THEscore && scoreNums !== 0) {
+    if (scoreNums < 500) narrate.textContent = "This is getting to easy huh? we shall go faster then";
+    else if (scoreNums < 1500) narrate.textContent = "Bruh whattt, go get job or something. Now IT IS FASTER";
+    else if (scoreNums === 1500) narrate.textContent = "Yeah idk if you are using bot or you just have a good gaming chair. Either way goodluck";
+    pvx += 1;
+    THEscore = scoreNums;
+  } else if (scoreNums === (THEscore + 5)) {
+    narrate.textContent = "";
+  }
 
   requestAnimationFrame(game);
 }
@@ -84,7 +103,7 @@ function jump() {
     isPlaying = true;
     requestAnimationFrame(game);
   }
-  vy = -5;
+  vy = -4;
   jumpSfx.currentTime = 0;
   jumpSfx.play();
 }
