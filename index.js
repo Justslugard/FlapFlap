86ac84ad7820016b171846750fbc61ad5fb27773
Justslugard <bot3445x@gmail.com>
2025-10-24 20:53:53 +0700

    make reset game without refreshing the page

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-let y = 200;
 let x = 200;
+let y = 200;
 let vy = 0;
 let pvx = 2;
 let isPlaying = false;
+let isOver = false;
 let THEscore = 0;
+let menuId;
 
 let pipes = [
    [400, Math.floor(Math.random() * 150)],
@@ -34,22 +36,25 @@ const levelUpSfx = new Audio();
 levelUpSfx.src = "audio/levelUp.mp3";
 
 // setInterval(game, 1000/30);
-ctx.font = "50px Pixelify Sans";
-ctx.fillText("Flap Flap Birdie", 25, 200);
 
-setInterval(() => {
-  ctx.font = "14px Pixelify Sans";
-  ctx.fillText("Press spacebar or mouse to start...", 25, 225);
-  setTimeout(() => {
-    ctx.clearRect(25, 210, 400, 400);
-  }, 1000)
-}, 1500)
+function menu() {
+  ctx.fillStyle = "black";
+  ctx.font = "50px Pixelify Sans";
+  ctx.fillText("Flap Flap Birdie", 25, 200);
 
-// setInterval(() => {
-// }, 1200)
+  menuId = setInterval(() => {
+    ctx.font = "14px Pixelify Sans";
+    ctx.fillText("Press spacebar or mouse to start...", 25, 225);
+    setTimeout(() => {
+      ctx.clearRect(25, 210, 400, 400);
+    }, 1000)
+  }, 1500)
+}
 
-let trueScore = 0;
+// let trueScore = 0;
+menu();
 function game() {
+  if (isOver) return;
   let scoreNums = Number(score.textContent.split(" ")[1]);
   ctx.clearRect(0, 0, canv.width, canv.height);
   vy += 0.3;
@@ -71,10 +76,14 @@ function game() {
     ctx.strokeRect(pipes[i][0] - 10, pipes[i][1] + 150, 70, 20);
 
     if (pipes[i][0] < 240 && pipes[i][0] > 160 && (pipes[i][1] > y || pipes[i][1] + 150 <= y + 30)) {
+      // alert("Birdie ded, Skill issue");
       overSfx.play();
-      alert("Birdie ded, Skill issue");
-      window.location.reload();
-      return;
+      isOver = true;
+      ctx.fillStyle = "black";
+      ctx.font = "25px Pixelify Sans";
+      ctx.fillText("Birdie ded, Diagnosis: Skill Issue", 0, 200);
+      ctx.font = "14px Pixelify Sans";
+      ctx.fillText("Press spacebar or mouse to restart...", 25, 225);
     }
 
     if (pipes[i][0] > (x - 17) && pipes[i][0] < x && !pipesChace.includes(pipes[i][1])) {
@@ -86,8 +95,8 @@ function game() {
     if (pipes[i][0] > -50) {
       pipes[i][0] -= pvx;
     } else {
-      trueScore += 1;
-      console.log(trueScore, scoreNums, `OFFSET: ${scoreNums - trueScore}`, `SPEED: ${pvx}`);
+      // trueScore += 1;
+      // console.log(trueScore, scoreNums, `OFFSET: ${scoreNums - trueScore}`, `SPEED: ${pvx}`);
       pipes[i][0] = 450;
       pipes[i][1] = Math.floor(Math.random() * 150);
     }
@@ -111,6 +120,27 @@ function game() {
   requestAnimationFrame(game);
 }
 
+function resetGame() {
+  ctx.clearRect(0, 0, canv.width, canv.height);
+  
+  x = y = 200;
+  vy = THEscore = 0;
+  pvx = 2;
+  isOver = isPlaying = false;
+  narrate.textContent = "";
+  score.textContent = "SCORE: 0";
+  score.style.display = "none";
+  pipes = [
+    [400, Math.floor(Math.random() * 150)],
+    [500, Math.floor(Math.random() * 150)],
+    [600, Math.floor(Math.random() * 150)],
+    [700, Math.floor(Math.random() * 150)],
+    [800, Math.floor(Math.random() * 150)]
+  ]
+
+  menu();
+}
+
 window.addEventListener("click", jump);
 window.addEventListener("keydown", e => {
   if (e.code == "Space") jump();
@@ -121,8 +151,12 @@ function jump() {
     isPlaying = true;
     requestAnimationFrame(game);
     score.style.display = "block";
+    clearInterval(menuId);
+  } else if (isOver) {
+    resetGame();
+  } else {
+    vy = -4;
+    jumpSfx.currentTime = 0;
+    jumpSfx.play();
   }
-  vy = -4;
-  jumpSfx.currentTime = 0;
-  jumpSfx.play();
 }
